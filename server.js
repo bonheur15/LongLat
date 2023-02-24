ba450bb4e08abd39080add720785d89d882c5d06
bonheur15 <35499564+bonheur15@users.noreply.github.com>
2023-02-24 04:59:06 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,32 +1,32 @@
 const express = require('express')
 const app = express()
-const ejs = require('ejs')
-const http = require('http')
-const addRouter = require('./Routers/add')
+
+
 const dbcontroller = require('./dbcontroller')
-// const bodyparser = require('body-parser')
 const bodyParser = require('body-parser')
 
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
+app.use(bodyParser.json())
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     console.log('Client on server')
     res.send('WELCOME')
 })
 
-app.use('/add', addRouter)
-app.use(bodyParser.json())
+app.get("/add", (req, res) => {
+    console.log(req.query.latitude);
+    
+    var latitude = req.query.latitude;
+    var alititude = req.query.alittude;
+    var longitude = req.query.longitude;
+    dbcontroller.add(latitude, longitude, alititude, () => {
+        console.log("Done");
+        res.send("Stored");
+    });
+});
 
 
-
-
-
-app.listen(3005, ()=>{
+app.listen(3005, () => {
     console.log('Server started at http://localhost:3005')
 })
 
-function test() {
-    
-}
-
-dbcontroller.add(2,1.2,1.1,test)
